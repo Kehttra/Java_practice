b7996b74fc46483d724c02692aee4444f5b46c5e
Wajeeh Daouk <91975573+Kehttra@users.noreply.github.com>
2022-08-08 23:49:05 +0300

    some declarations of the Jframe nothing advanced

--- GUI_java/src/App.java
+++ GUI_java/src/App.java
@@ -1,5 +1,22 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int _width = 700, _height = 500;
+        String _mainTitle = "Java Test GUI";
+
+
+
+        JFrame _mainFrame = new JFrame();
+        _mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //_mainFrame.setResizable(true);
+
+        _mainFrame.setTitle(_mainTitle);
+        _mainFrame.setSize(_width, _height);
+        _mainFrame.setVisible(true);
+
+        
     }
 }
