436f2f3c0f0ce0ea6d03227efb338c1a4b0bd33b
Wajeeh Daouk <91975573+Kehttra@users.noreply.github.com>
2022-08-10 04:00:15 +0300

    multi-tiering of code

--- GUI_java/src/App.java
+++ GUI_java/src/App.java
@@ -1,22 +1,10 @@
-import javax.swing.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int _width = 700, _height = 500;
-        String _mainTitle = "Java Test GUI";
-
-
-
-        JFrame _mainFrame = new JFrame();
-        _mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Create and set the main stuff
         
-        //_mainFrame.setResizable(true);
-
-        _mainFrame.setTitle(_mainTitle);
-        _mainFrame.setSize(_width, _height);
-        _mainFrame.setVisible(true);
-
         
+        Myframe _mainMyframe = new Myframe();
+        _mainMyframe._visibleFrame = true;
     }
 }
---
+++ GUI_java/src/Myframe.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Myframe extends JFrame{
+    
+     //Variable declaractions
+        //Can be changed by user later on
+        int _width = 700, _height = 500;
+        String _mainTitle = "Java Test GUI";
+        ImageIcon _logo = new ImageIcon("images/orb_image.png");
+        boolean _resize = true;
+        boolean _visibleFrame = false;
+        Color _color = new Color(0,0,0);
+
+
+
+    Myframe()
+    {
+
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //this.setResizable(true);
+
+        this.setTitle(_mainTitle);
+        this.setSize(_width, _height);
+        this.setVisible(_visibleFrame);
+        this.setResizable(_resize);
+        //Setting the Icon of the main frame
+        
+        this.setIconImage(_logo.getImage());
+        this.getContentPane().setBackground(_color);
+    }
+
+    
+
+}
